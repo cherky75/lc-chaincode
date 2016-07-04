2d395f09eb837d15d79fdeec2f9dbe797f592d6e
Chanhee Han <cherky@empal.com>
2016-07-04 10:42:58 +0900

    no message

--- lc-chaincode.go
+++ lc-chaincode.go
@@ -27,11 +27,6 @@ import (
 type SimpleChaincode struct {
 }
 
-//var lcIndexStr = "_lcIndex"
-
-// ============================================================================================================================
-// Main
-// ============================================================================================================================
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -41,30 +36,11 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-
-	//	var Aval int
-	var err error
-
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	// Initialize the chaincode
-	//	Aval, err = strconv.Atoi(args[0])
-	//	if err != nil {
-	//		return nil, errors.New(("Expecting integer value for asset holding"))
-	//	}
-
-	//	err = stub.PutState("abc", []byte(strconv.Itoa(Aval)))
-	//	if err != nil {
-	//		return nil, err
-	//	}
-
-	//	var empty []string
-	//	jsonAsBytes, _ := json.Marshal(empty)
-	//	err = stub.PutState(_lcIndexStr, jsonAsBytes)
-
-	err = stub.PutState("hello_world", []byte(args[0]))
+	err := stub.PutState("hello_world", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
@@ -72,17 +48,17 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	return nil, nil
 }
 
-// Invoke is our entry point to invoke a chaincode function
+// Invoke isur entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
-	if function == "init" { //initialize the chaincode state, used as reset
+	if function == "init" {
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
-
+		return t.write(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function) //error
+	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation")
 }
@@ -92,29 +68,48 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "read" {
-		fmt.Println("read function called")
+	if function == "read" { //read a variable
 		return t.read(stub, args)
 	}
-
-	fmt.Println("query did not find func: " + function) //error
+	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query")
 }
 
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var name, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
+	}
+
+	name = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(name, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var jsonResp string
+	var name, jsonResp string
 	var err error
 
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments, Expecting name of the var to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
 	}
 
-	key := args[0]
-	valAsbytes, err := stub.GetState(key)
+	name = args[0]
+	valAsbytes, err := stub.GetState(name)
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
 		return nil, errors.New(jsonResp)
 	}
+
 	return valAsbytes, nil
 }
