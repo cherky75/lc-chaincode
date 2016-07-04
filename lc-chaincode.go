82651fb388325de934e2dc61cbe715887e77d5d2
Chanhee Han <cherky@empal.com>
2016-07-04 14:46:57 +0900

    no message

--- lc-chaincode.go
+++ lc-chaincode.go
@@ -66,7 +66,10 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "create" {
+		return t.create(stub, args)
 	}
+
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation")
